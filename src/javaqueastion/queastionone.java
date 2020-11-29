28f094fa73983d66e14e9420357d8daa2b643154
yimers <73725124+yimers@users.noreply.github.com>
2020-11-29 16:36:12 -0500

    this is java project one

---
+++ src/javaqueastion/queastionone.java
@@ -0,0 +1,10 @@
+package javaqueastion;
+
+public class queastionone {
+    public static void main(String[] args) {
+
+        System.out.println( "hello java");
+        System.out.println(" hello sirgut");
+
+    }
+}
---
+++ src/javaqueastion/queastiontwo.java
@@ -0,0 +1,11 @@
+package javaqueastion;
+
+public class queastiontwo {
+    public static void main(String[] args) {
+        System.out.println("salary");
+        System.out.println("i love noah");
+        System.out.println("i love Maya");
+        System.out.println("i love java");
+
+    }
+}
