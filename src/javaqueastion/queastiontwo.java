792017ffbc34c8500510935c8d4ada7a7282bbcc
yimers <73725124+yimers@users.noreply.github.com>
2020-11-29 16:48:26 -0500

    this is first update

--- src/javaqueastion/queastiontwo.java
+++ src/javaqueastion/queastiontwo.java
@@ -2,7 +2,7 @@ package javaqueastion;
 
 public class queastiontwo {
     public static void main(String[] args) {
-        System.out.println("salary");
+
         System.out.println("i love noah");
         System.out.println("i love Maya");
         System.out.println("i love java");
